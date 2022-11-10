f787620bfa1faada83689b55c46c999bedd49def
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-11-10 10:34:51 +0100

    Mise en place des routes + navbar terminé

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import {useRoutes} from 'react-router-dom';
+import Footer from "./components/Footer/Footer";
+import routes from './services/Routes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    const routing = useRoutes(routes);
+    return (
+        <>
+            <Navbar/>
+            {routing}
+            <Footer/>
+        </>
+    )
 }
 
 export default App
