f61bbeaa2e5146ff92026c4edf089fabdcbc2675
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2023-01-03 01:33:18 +0100

    Navbar retravaillé avec tailwind + responsive effectué

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,83 +1,80 @@
-import React, {useState} from 'react'
-import {Link, To} from 'react-router-dom'
-import routes from "../../services/routes";
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../services/routes';
 
 const Navbar = () => {
-    const [isModalOpen, setModalOpen] = useState(false)
+    const [isModalOpen, setModalOpen] = useState(false);
     return (
-        <div className="flex container w-full mx-auto justify-between items-center font-bold text-2xl px-5  py-6">
+        <div className="flex container w-full mx-auto justify-between items-center font-bold text-2xl px-5 py-6">
             <Link to="/">
-                <h1 className="h-[3.5rem] flex items-center text-center">Logo</h1>
+                <h1 className="h-3.5rem flex items-center text-center">Logo</h1>
             </Link>
             <nav className="hidden md:flex space-x-10 items-center">
-                {routes.map((n) => {
+                {routes.map((route) => {
                     return (
-                        <Link key={n.name} to={n.path as To}>
-                            {n.name}
+                        <Link key={route.name} to={route.path}>
+                            {route.name}
                         </Link>
-                    )
+                    );
                 })}
             </nav>
             <div className="md:hidden">
                 {isModalOpen ? (
                     <div
-                        className="bg-[#161B21] border-[1px] border-red-100/20  fixed w-[24rem] right-0 top-0 h-[100vh] transition translate-x-[-1px] rounded-lg  text-white">
+                        className="bg-#161B21 border-1px border-red-100 fixed w-24rem right-0 top-0 h-100vh transition translate-x-1px rounded-lg text-white"
+                    >
                         <div className="pt-5 pb-6 px-5">
-                            <button onClick={() => setModalOpen(!isModalOpen)} className="-mr-2 float-right mb-3">
+                            <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={() => setModalOpen(!isModalOpen)}>
                                 <svg
+                                    className="w-6 h-6"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    aria-hidden="true"
-                                    className="h-6 w-6"
-                                    viewBox="0 0 24 24"
                                 >
                                     <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    ></path>
+                                        fillRule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
                                 </svg>
                             </button>
 
-                            <div className="mt-[5rem] z-10 relative">
-                                <nav className="grid gap-y-8">
-                                    {navData.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.path}
-                                            className="-m-3 p-3 flex items-center rounded-md hover:bg-black/20  border-[1px] border-gray-500/60"
+                            <div className="mt-5rem z-10 relative">
+                                <div className="absolute right-0 mt-2 py-2 bg-white rounded-md shadow-xl">
+                                    {routes.map((route) => (
+                                        <Link
+                                            key={route.name}
+                                            to={route.path}
+                                            className="block px-4 py-2 text-gray-800 rounded-md hover:bg-gray-200"
                                         >
-                                            <h1 className="my-3 ml-3 text-3xl font-bold ">{item.name}</h1>
-                                        </a>
+                                            <h1 className="">
+                                                {route.name}
+                                            </h1>
+                                        </Link>
                                     ))}
-                                </nav>
+                                </div>
                             </div>
                         </div>
                     </div>
                 ) : (
-                    <button onClick={() => setModalOpen(!isModalOpen)}>
+                    <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={() => setModalOpen(!isModalOpen)}>
                         <svg
+                            className="w-6 h-6"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="white"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
                         >
                             <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M4 6h16M4 12h16m-7 6h7"
+                                fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clipRule="evenodd"
                             />
                         </svg>
                     </button>
                 )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/features/smashApi.ts
+++ src/features/smashApi.ts
@@ -8,22 +8,12 @@ export const fetchVodByCharactersId = async (id1: string, id2?: string) => {
         character1: id1,
         ...(id2 ? {character2: id2} : {}),
     }))
-        .then(res => (res.json()))
+        .then(res => res.json())
         .then(data => data.items)
 }
 
 export const fetchCharacters = async () => {
     return fetch(API_CHARA_LIST)
-    .then(res => (res.json()))
+    .then(res => res.json())
     .then(data => data)
-}
-
-export const fetchVodByCharactersIdAlt = async (id1: string, id2?: string) => {
-    const response = await fetch(API_VOD + new URLSearchParams({
-        character1: id1,
-        ...(id2 ? {character2: id2} : {}),
-    }))
-    const responseJson = await response.json()
-
-    return responseJson.items
 }
