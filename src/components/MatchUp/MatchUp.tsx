077adc50b7c617bab4ee97b0e66a35bd91313771
Illyes <illyeszerga645@gmail.com>
2024-03-26 12:47:59 +0100

    Changement des class des li afin de rendre cela plus propre

--- src/components/MatchUp/MatchUp.tsx
+++ src/components/MatchUp/MatchUp.tsx
@@ -23,13 +23,12 @@ const MatchUp = () => {
                     specifique.</p>
                 <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
                     {characters.map(character => (
-                        <li key={character.id} className="col-span-1 p-4 bg-gray-200 rounded-lg shadow-lg">
+                        <li key={character.id} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
                             <img src={`../../src/assets/smashbrosicon/${character.id}.png`} alt={character.name}
                                  className="h-32 w-32 object-cover rounded-full mx-auto mb-4"/>
                             <h2 className="text-xl font-bold mb-2 text-center">{character.name}</h2>
                             <input type={"checkbox"}
                                    className="px-4 py-2 rounded-full bg-red-600 text-white font-bold hover:bg-red-700">
-
                             </input>
                         </li>
                     ))}
