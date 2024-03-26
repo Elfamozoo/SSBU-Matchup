0113c8cb0d9007be6bdadbe1c9a2ac5967fe7052
Illyes <illyeszerga645@gmail.com>
2024-03-26 13:08:20 +0100

    Ajout Titre et presentation a l'accueil

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,17 @@
 const Home = () => {
     return (
-        <h1 className="text-5xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                <span className="text-yellow-600 dark:text-yellow-500">MATCH UP </span>Explorez, Apprenez, Dominez!
+            </h1>
+            <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400">Une collection de vidéos à la
+                demande (VODs)
+                de Super Smash Bros. Ultimate sélectionnées à partir de
+                différentes
+                chaînes YouTube. Consultez par personnage ou affrontement pour trouver les images parfaites pour vous
+                aider à
+                maîtriser votre personnage ou à suivre les tournois. Le site s'appelle Match Up !</p>
+        </>
     )
 }
 
