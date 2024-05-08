a018f411a9f7e54026d23bca4d882f0638e7f77f
Illyes <illyeszerga645@gmail.com>
2024-05-09 00:24:04 +0200

    Projet en v1.0 terminé Systeme de pagination retravaillé afin d'y inclure une limitation de pages qui s'affiche en 5 par 5 et ajout de la variable isActive qui gere sur quel page on se situe.

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,11 @@
 import {useLocation, useNavigate} from "react-router-dom";
+import {
+    ChevronDoubleLeftIcon,
+    ChevronDoubleRightIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon
+} from '@heroicons/react/20/solid'
+
 const Pagination = (props: { countResults: number }) => {
     const calculatePages = () => {
         return Math.ceil(props.countResults / 10);
@@ -19,43 +26,74 @@ const Pagination = (props: { countResults: number }) => {
     const isDisablePrevious = currentPageNumber == 1;
     const isDisableNext = currentPageNumber == pagesTotal
 
+    const getPagination = () => {
+        let pagesLimit: number[] = []
+        if (currentPageNumber == 1) {
+            pagesLimit = [currentPageNumber, currentPageNumber + 1, currentPageNumber + 2, currentPageNumber + 3, currentPageNumber + 4]
+        } else if (currentPageNumber == 2) {
+            pagesLimit = [currentPageNumber - 1, currentPageNumber, currentPageNumber + 1, currentPageNumber + 2, currentPageNumber + 3]
+        } else if (currentPageNumber == pagesTotal) {
+            pagesLimit = [currentPageNumber - 4, currentPageNumber - 3, currentPageNumber - 2, currentPageNumber - 1, currentPageNumber]
+        } else {
+            pagesLimit = [currentPageNumber - 2, currentPageNumber - 1, currentPageNumber, currentPageNumber + 1, currentPageNumber + 2]
+        }
+        return {
+            firstPage: 1,
+            lastPage: pagesTotal,
+            pages: pagesLimit,
+            previousPage: currentPageNumber - 1,
+            nextPage: currentPageNumber + 1,
+        }
+    }
+    const buttonsNavigation = getPagination()
+
     return (
         <>
-            <nav className="flex justify-between items-center gap-x-1">
-                <button disabled={isDisablePrevious} onClick={() => {
-                    navigate(`${location.pathname}?page=${currentPageNumber - 1}`)
-                }} type="button"
-                        className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex jusify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
-                    <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                         viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                         strokeLinejoin="round"
-                         data-darkreader-inline-stroke="">
-                        <path d="m15 18-6-6 6-6"></path>
-                    </svg>
-                    <span aria-hidden="true" className="hidden sm:block">Precedent</span>
+            <nav className="flex justify-center items-center -space-x-px">
+                <button
+                    onClick={() => {
+                        navigate(`${location.pathname}?page=${buttonsNavigation.firstPage}`)
+                    }}
+                    type="button"
+                    className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+                    <ChevronDoubleLeftIcon/>
                 </button>
-                <div className="flex items-center gap-x-1">
-                    {pages.map(page => (
-                        
-                        <button onClick={() => {
+                <button
+                    disabled={isDisablePrevious}
+                    onClick={() => {
+                        navigate(`${location.pathname}?page=${buttonsNavigation.previousPage}`)
+                    }}
+                    type="button"
+                    className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+                    <ChevronLeftIcon/>
+                </button>
+                {buttonsNavigation.pages.map(page => {
+                    const isActive = currentPageNumber == page
+                    return <button
+                        onClick={() => {
                             navigate(`${location.pathname}?page=${page}`)
-                        }} type="button"
-                                className="min-h-[38px] min-w-[38px] flex justify-center items-center bg-gray-200 text-gray-800 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-600 dark:text-white dark:focus:bg-neutral-500"
-                                aria-current="page">{page}
-                        </button>
-                    ))}
-                </div>
-                <button disabled={isDisableNext} onClick={() => {
-                    navigate(`${location.pathname}?page=${currentPageNumber + 1}`)
-                }} type="button"
-                        className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex jusify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
-                    <span aria-hidden="true" className="hidden sm:block">Suivant</span>
-                    <svg className="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                         viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                         strokeLinejoin="round"
-                         data-darkreader-inline-stroke="">
-                        <path d="m9 18 6-6-6-6"></path>
-                    </svg>
+                        }}
+                        type="button"
+                        className={`${isActive ? 'min-h-[38px] min-w-[38px] flex justify-center items-center bg-gray-200 text-gray-800 border border-gray-200 py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg focus:outline-none focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-600 dark:border-neutral-700 dark:text-white dark:focus:bg-neutral-500' : 'min-h-[38px] min-w-[38px] flex justify-center items-center border border-gray-200 text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10'}`}
+                        aria-current="page">{page}
+                    </button>
+                })}
+                <button
+                    disabled={isDisableNext}
+                    onClick={() => {
+                        navigate(`${location.pathname}?page=${buttonsNavigation.nextPage}`)
+                    }}
+                    type="button"
+                    className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+                    <ChevronRightIcon/>
+                </button>
+                <button
+                    onClick={() => {
+                        navigate(`${location.pathname}?page=${buttonsNavigation.lastPage}`)
+                    }}
+                    type="button"
+                    className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+                    <ChevronDoubleRightIcon/>
                 </button>
             </nav>
         </>
